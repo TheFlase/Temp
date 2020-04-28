f55a4e9cfc012ee56c46bf4589755e399d161685
theFlase <wengguochao@163.com>
2020-04-28 13:48:01 +0800

    自定义classloader并简单测试

---
+++ src/main/java/com/wgc/concurrent/auto/Hello4ClassLoader.java
@@ -0,0 +1,12 @@
+package com.wgc.concurrent.auto;
+
+/**
+ * @Author wgc
+ * @Description //TODO
+ * @Date 4/23/2020
+ **/
+public class Hello4ClassLoader {
+    public void say(){
+        System.out.println("Hello ClassLoader!");
+    }
+}
