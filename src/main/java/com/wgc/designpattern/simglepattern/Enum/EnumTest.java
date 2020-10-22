1dc7df6eceb877e5c5a349babd3078f2ffadbe7a
theFlase <wengguochao@163.com>
2020-10-22 23:26:17 +0800

    单例模式

---
+++ src/main/java/com/wgc/designpattern/simglepattern/Enum/EnumTest.java
@@ -0,0 +1,12 @@
+package com.wgc.designpattern.simglepattern.Enum;
+
+/**
+ * @Author wgc
+ * @Description //测试枚举单例
+ * @Date 2020/10/21
+ **/
+public class EnumTest {
+    public static void main(String[] args) {
+
+    }
+}
